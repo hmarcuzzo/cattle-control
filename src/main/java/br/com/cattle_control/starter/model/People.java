eccd9fb52d6de5e1df7c5480a928cff28791679f
hmarcuzzo <henrique.souza.m06@gmail.com>
2020-08-30 03:10:13 -0300

    people with lombok

--- src/main/java/br/com/cattle_control/starter/model/People.java
+++ src/main/java/br/com/cattle_control/starter/model/People.java
@@ -2,165 +2,60 @@ package br.com.cattle_control.starter.model;
 
 import java.io.Serializable;
 
-public class People implements Serializable, Comparable<People>{
-    
-    private Integer id;
-    private Integer type;
-    private Integer id_type;
-    private String  name;
-    private String  email;
-    private Integer phone;
-    private String  info;
-    private Boolean deleted;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.persistence.Entity;
+// import java.util.Set;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "PEOPLE")
+public class People implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "PEOPLE_ID")
+    private int id;
+
+    @Column(name = "PEOPLE_NAME")
+    @NotEmpty(message = "*Por favor forneça o nome")
+    private String name;
+
+    @Column(name = "EMAIL")
+    @Email(message = "*Por favor forneça um e-mail válido")
+    private String email;
+
+    @Column(name = "TYPE")
+    @NotEmpty(message = "*Por favor defina o tipo")
+    private int type;
+
+    @Column(name = "ID_TYPE")
+    @Length(min = 11, max= 14, message = "*Por favor forneça todos os dígitos do CPF")
+    @NotEmpty(message = "*Por favor forneça um CPF")
+    private String idType;
 
 
-    public People() {
-    }
+    @Column(name = "PHONE")
+    @NotEmpty(message = "*Por favor forneça o telefone")
+    @Length(min = 10, max= 25, message = "*Por favor forneça todos os dígitos do telefone")
+    private String phone;
 
-    public People(Integer id) {
-        this.id = id;
-    }
+    @Column(name = "PEOPLE_INFO")
+    @Length(min = 0, max= 1000, message = "*A descrição deve ter no máximo 1000 caracteres")
+    private String info;
 
-    public Integer getId() {
-        return id;
-    }
+    // @OneToMany
+    // @JoinTable(name = "PEOPLE_ROLE", joinColumns = @JoinColumn(name = "PEOPLE_ID"), inverseJoinColumns = @JoinColumn(name = "ROLE_ID"))
+    // private Set<Role> roles;
 
-    public Integer getType() {
-        return type;
-    }
-
-    public Integer getIdType() {
-        return id_type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Integer getPhone() {
-        return phone;
-    }
-
-    public String getInfo() {
-        return info;
-    }
-
-    public Boolean getDeleted() {
-        return deleted;
-    }
-
-
-    
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    public void setIdType(Integer id_type) {
-        this.id_type = id_type;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(Integer phone) {
-        this.phone = phone;
-    }
-
-    public void setInfo(String info) {
-        this.info = info;
-    }
-
-    public void setDeleted(Boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    
-
-    public People type(Integer type) {
-        this.type = type;
-        return this;
-    }
-
-    public People idType(Integer id_type) {
-        this.id_type = id_type;
-        return this;
-    }
-
-    public People name(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public People email(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public People phone(Integer phone) {
-        this.phone = phone;
-        return this;
-    }
-
-    public People info(String info) {
-        this.info = info;
-        return this;
-    }
-
-    public People deleted(Boolean deleted) {
-        this.deleted = deleted;
-        return this;
-    }
-
-
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        People other = (People) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
-    public boolean hasModel() {
-        return model != null && !"".equals(model.trim());
-    }
-
-    public boolean hasName() {
-        return name != null && !"".equals(name.trim());
-    }
-
-    @Override
-    public int compareTo(People o) {
-        return this.id.compareTo(o.getId());
-    }
 }
