7fe77bddcf7293d63f5c9c92627e0efe63394218
hmarcuzzo <henrique.souza.m06@gmail.com>
2020-09-08 01:16:49 -0300

    removendo carros

--- src/main/java/br/com/cattle_control/starter/view/PeopleListView.java
+++ src/main/java/br/com/cattle_control/starter/view/PeopleListView.java
@@ -1,26 +1,14 @@
 package br.com.cattle_control.starter.view;
 
-import javax.faces.view.ViewScoped;
-import org.omnifaces.util.Faces;
-
-import java.io.Console;
-// import javax.inject.Inject;
-// import javax.inject.Named;
-import java.io.IOException;
 
 import java.util.List;
-import java.util.Map;
-// import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.annotation.PostConstruct;
 
 
 import lombok.RequiredArgsConstructor;
 	
-// import org.primefaces.model.DefaultStreamedContent;
-// import org.primefaces.model.StreamedContent;
+
 import org.primefaces.model.LazyDataModel;
-import org.primefaces.model.SortOrder;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
@@ -30,11 +18,11 @@ import br.com.cattle_control.starter.exception.AnyPersistenceException;
 import br.com.cattle_control.starter.exception.EntityAlreadyExistsException;
 import br.com.cattle_control.starter.service.PeopleService;
 import br.com.cattle_control.starter.infra.model.Filter;
+
 import com.github.adminfaces.template.exception.BusinessException;
 
 
 import static br.com.cattle_control.starter.util.Utils.addDetailMessage;
-import static com.github.adminfaces.template.util.Assert.has;
 
 @Component
 @RequestScope
